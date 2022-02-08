07a7204bf791b8f793c5574a83f8a998ad3800dd
cc <votaryofturms@icloud.com>
2022-02-08 02:12:12 -0500

    get users endpoint

--- api/server.js
+++ api/server.js
@@ -1,6 +1,25 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-
+const User = require('./users/model')
 const server = express()
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error',
+                err: err.message
+            })
+        })
+})
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
